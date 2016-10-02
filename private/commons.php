7ebdb4875a1915d5be8b779eb769471ab5fc9352
indeep-xyz@gmail.com <indeep.xyz@gmail.com>
2016-10-02 15:01:29 +0900

    Add a definition for message example

--- private/commons.php
+++ private/commons.php
@@ -2,5 +2,6 @@
 
 define('__PRIVATE_ROOT__', dirname(__FILE__));
 define('__CLASS_ROOT__', __PRIVATE_ROOT__ . '/class');
+define('__EXAMPLE_ROOT__', __PRIVATE_ROOT__ . '/example');
 
 require_once(__PRIVATE_ROOT__ . '/config.php');
