980b559e4bba26a4e49a46b577fbb5ca73542388
indeep-xyz@gmail.com <indeep.xyz@gmail.com>
2016-10-12 08:21:34 +0900

    Fix a variable name in public/callback.php

--- public/callback.php
+++ public/callback.php
@@ -4,12 +4,12 @@ require_once('../private/commons.php');
 require_once(__CLASS_ROOT__ . '/line-agent/reception.php');
 
 $jsonSource = file_get_contents("php://input");
-$agent = new \LineAgent\Reception($jsonSource);
-$replier = $agent->getReplier();
+$reception = new \LineAgent\Reception($jsonSource);
+$replier = $reception->getReplier();
 
 if ($replier === null) {
-  $agent = new \LineAgent\Reception(file_get_contents(__EXAMPLE_ROOT__ . '/text-message-en'));
-  $replier = $agent->getReplier();
+  $reception = new \LineAgent\Reception(file_get_contents(__EXAMPLE_ROOT__ . '/text-message-en'));
+  $replier = $reception->getReplier();
   $replier->setDryRun(true);
 }
 
