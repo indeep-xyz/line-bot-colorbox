31eb771d40f4a91e2c383df5247904a257dfaa28
indeep-xyz <indeep.xyz@gmail.com>
2016-12-01 16:25:54 +0900

    Prepare to include log4php

--- private/commons.php
+++ private/commons.php
@@ -4,4 +4,5 @@ define('__PRIVATE_ROOT__', dirname(__FILE__));
 define('__CLASS_ROOT__', __PRIVATE_ROOT__ . '/class');
 define('__EXAMPLE_ROOT__', __PRIVATE_ROOT__ . '/example');
 
+require_once(__PRIVATE_ROOT__ . '/lib/log4php/Logger.php');
 require_once(__PRIVATE_ROOT__ . '/config.php');
