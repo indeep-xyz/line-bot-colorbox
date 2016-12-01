62d3a4c6c403076f39fbc53f11cd58883a3cb4fc
indeep-xyz <indeep.xyz@gmail.com>
2016-12-01 16:21:28 +0900

    Update callback.php according to LineAgent
    - Update it.
    - Add try-catch statement
    - Add writing error log

--- public/callback.php
+++ public/callback.php
@@ -3,19 +3,26 @@
 require_once('../private/commons.php');
 require_once(__CLASS_ROOT__ . '/line-agent/reception.php');
 
-$jsonSource = file_get_contents("php://input");
-$reception = new \LineAgent\Reception($jsonSource);
-$replier = $reception->getReplier();
-
-if ($replier === null) {
-  $reception = new \LineAgent\Reception(file_get_contents(__EXAMPLE_ROOT__ . '/text-message-en'));
+try {
+  $jsonSource = file_get_contents("php://input");
+  $reception = new \LineAgent\Reception($jsonSource);
   $replier = $reception->getReplier();
-  $replier->setDryRun(true);
-}
 
-if ($replier->hasFrom()) {
-  $replier->send();
-}
-else {
-  $replier->printBody();
+  // file_put_contents("log", "jsonSource" . $jsonSource . "\n", FILE_APPEND);
+
+  if ($replier === null) {
+    $reception = new \LineAgent\Reception(file_get_contents(__EXAMPLE_ROOT__ . '/text-message-en'));
+    $replier = $reception->getReplier();
+    $replier->setDryRun(true);
+  }
+
+  if ($replier->hasReplyToken()) {
+    $replier->send();
+  }
+  else {
+    $replier->printBody();
+  }
+
+} catch (Exception $e) {
+  // file_put_contents("log", $e->getTraceAsString() . "\n", FILE_APPEND);
 }
