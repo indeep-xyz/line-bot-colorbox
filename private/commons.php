f852f70e59b5fcf92f96aef7bd035070c082ccb5
indeep-xyz <indeep.xyz@gmail.com>
2016-12-02 15:13:15 +0900

    Set internal encording at first

--- private/commons.php
+++ private/commons.php
@@ -6,3 +6,5 @@ define('__EXAMPLE_ROOT__', __PRIVATE_ROOT__ . '/example');
 
 require_once(__PRIVATE_ROOT__ . '/lib/log4php/Logger.php');
 require_once(__PRIVATE_ROOT__ . '/config.php');
+
+mb_internal_encoding("UTF-8");
