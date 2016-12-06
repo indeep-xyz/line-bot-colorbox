6ebb9e793285fcbcc5a306ad02465ed0a2977e36
indeep-xyz <indeep.xyz@gmail.com>
2016-12-06 18:24:37 +0900

    Employ the logger into public/box.php

--- public/box.php
+++ public/box.php
@@ -3,12 +3,21 @@
 require_once('../private/commons.php');
 require_once(__CLASS_ROOT__ . '/color-box/out.php');
 
-$colorSource = $_GET['color'];
-$colorManager = new \ColorBox\ColorManager($colorSource);
+\MyLocalLogger\Write::journal('------ START ------');
 
-if ($colorManager->isCreatedColor()) {
-  $color = $colorManager->getColor();
+try {
+  $colorSource = $_GET['color'];
+  $colorManager = new \ColorBox\ColorManager($colorSource);
 
-  $box = new \ColorBox\Out($color);
-  $box->output();
+  if ($colorManager->isCreatedColor()) {
+    $color = $colorManager->getColor();
+
+    $box = new \ColorBox\Out($color);
+    $box->output();
+  }
 }
+catch (Exception $ex) {
+  \MyLocalLogger\Write::error('Something error', $ex);
+}
+
+\MyLocalLogger\Write::journal('------- END -------');
