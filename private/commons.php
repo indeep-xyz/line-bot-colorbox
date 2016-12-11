82ec7753f7a2098437fbdbf1293f9743cd718c0d
indeep-xyz <indeep.xyz@gmail.com>
2016-12-12 06:19:32 +0900

    Write some logs only in the debug mode
    - Update MyLocalLogger to v0.1.1.
    - Write some logs, debug and journal, only in the debug mode.
    - The mode, debug or not, depends on the file "private/DEBUG_MODE" exists.

--- private/commons.php
+++ private/commons.php
@@ -6,10 +6,14 @@ define('__EXAMPLE_ROOT__', __PRIVATE_ROOT__ . '/example');
 
 require_once(__PRIVATE_ROOT__ . '/lib/log4php/Logger.php');
 require_once(__PRIVATE_ROOT__ . '/config.php');
+require_once(__PRIVATE_ROOT__ . '/lib/MyLocalLogger/Configure.php');
 require_once(__PRIVATE_ROOT__ . '/lib/MyLocalLogger/Write.php');
-require_once(__PRIVATE_ROOT__ . '/lib/MyLocalLogger/LoggerAgent.php');
 
 date_default_timezone_set('Asia/Tokyo');
 mb_internal_encoding("UTF-8");
 
-\MyLocalLogger\LoggerAgent::setLogDir(dirname(__FILE__) . '/log');
+\MyLocalLogger\Configure::setLogDir(dirname(__FILE__) . '/log');
+
+if (!file_exists(dirname(__FILE__) . '/DEBUG_MODE')) {
+  \MyLocalLogger\Configure::setMockNames(['debug' , 'journal']);
+}
